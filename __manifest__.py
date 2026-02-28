cc70aa3eea4990eff6230f0ec83b45b3e669994f
jardel.bernardo <jardel.bernardo@alien-group.com>
2026-02-28 22:36:06 +0100

    [NEW] add a security and update manifest file
    Took 13 minutes

--- __manifest__.py
+++ __manifest__.py
@@ -3,6 +3,18 @@
     "version": "1.0.0",
     "odoo_version": "18.0.0",
     "summary": "Agendamento online de atendimento",
+    "description": "",
+    "author": "Jardel Elias Bernardo",
+    'website': "https://github.com/odoomakers/ticket_scheduling",
     "depends": ['web', 'contacts'],
-    'data': []
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'assets': {
+        'web.assets_frontent':[],
+        'web.assets_backend': []
+    },
+
+    'application': True,
+    "installable": True
 }
